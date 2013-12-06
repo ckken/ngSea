72cf011eb753292a1740d34809a49ca45aead32c
ken <ckken@qq.com>
2013-12-06 13:57:03 +0800

    Update angular-ngSea.js

--- app/angularjs/angular-ngSea.js
+++ app/angularjs/angular-ngSea.js
@@ -1,13 +1,15 @@
 /**
- * ngSea by KenZR email ckken@qq.com
+ * ngSea VerSion 0.2.1
+ * Author by KenZR email ckken@qq.com
  * Create time 2013/12/5
- * support in IE6 with  the Angular 1.0.8 (because the Angular 1.2.3 not support in lt IE 7)
- * use it inject ngSea And run use app = $ngSea(app); have fun
+ * support in IE6 about with the Angular 1.0.8 (because the 1.2.3 not support in IE 7)
+ * use it inject ngSea And in run use app = $ngSea(app); that's all
  * Contact us: QQ 117692258
  */
 angular.module('ngSea', [], ["$controllerProvider", "$compileProvider", "$filterProvider", "$provide", function ($controllerProvider, $compileProvider, $filterProvider, $provide) {
     $provide.factory('$ngSea', ['$rootScope', '$q', function ($rootScope, $q) {
         return function (app) {
+
             $rootScope.activeApply = function (fn) {
                 var phase = this.$root.$$phase;
                 if (phase == '$apply' || phase == '$digest') {
@@ -18,17 +20,12 @@ angular.module('ngSea', [], ["$controllerProvider", "$compileProvider", "$filter
                     this.$apply(fn);
                 }
             };
+
             $rootScope.$on('$routeChangeStart', function (e, target) {
-                var route = target && target.$$route;
-                route.resolve = route.resolve || {};
+                var route = target && target.$$route ||{};
+                route.resolve =route.resolve || {};
                 route.resolve.loadedModule = function () {
                     var deferred = $q.defer();
-                    /*require.async(route.controllerUrl, function (m) {
-                        $rootScope.activeApply(function () {
-                            deferred.resolve(angular.isFunction(m) ? m(app) : m);
-                        });
-                    });*/
-
                     seajs.use(route.controllerUrl, function(m){
                         $rootScope.activeApply(function () {
                             if(angular.isUndefined(m)){
@@ -42,6 +39,8 @@ angular.module('ngSea', [], ["$controllerProvider", "$compileProvider", "$filter
                     return deferred.promise;
                 }
             });
+
+
             app.register = {
                 controller: $controllerProvider.register,
                 directive: $compileProvider.directive,
